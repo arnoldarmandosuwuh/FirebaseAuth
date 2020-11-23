13633067d4272c7b013738a3418fdda1baa8a389
Arnold Armando Suwuh <arnoldarmando07@gmail.com>
2020-11-23 21:00:27 +0700

    finish

--- src/config/Fire/index.js
+++ src/config/Fire/index.js
@@ -1,14 +1,14 @@
 import firebase from 'firebase'
 
 firebase.initializeApp({
-  apiKey: 'AIzaSyDtPoC3xjN9JIAngFb3q7ClgGe3E38mnYs',
-  authDomain: 'my-doctor-07.firebaseapp.com',
-  databaseURL: 'https://my-doctor-07.firebaseio.com',
-  projectId: 'my-doctor-07',
-  storageBucket: 'my-doctor-07.appspot.com',
-  messagingSenderId: '181398289592',
-  appId: '1:181398289592:web:83f0ca6950bd7ff23fc84d',
-  measurementId: 'G-T2V6KP8SH2',
+  apiKey: '',
+  authDomain: '',
+  databaseURL: '',
+  projectId: '',
+  storageBucket: '',
+  messagingSenderId: '',
+  appId: '',
+  measurementId: '',
 })
 
 const Fire = firebase
